19c77f24b79cba7ab70c26d86cb971cce32e8bef
YhinkaDevOps <yinkabakare30@gmail.com>
2022-08-28 07:19:26 +0100

    fetched data from omdb with api key using fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import "./App.css"
 
-function App() {
- 
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=c3fe43d9"
+    function App() {
+      const searchMovies = async(title) =>{
+        const response = await fetch(`${API_URL}&s={title}`)
+        const data = await response.json()
+          console.log(data.Search)
+      }
+      useEffect(()=>{
+        searchMovies('thor')
+      },[])
+      // fetch(API_URL)
+      // .then((response)=>{
+      //     return response.json()
+      // }).then((response)=>{
+      //   console.log(response)
+      // })
+
   return (
     <div className="App">
 
